547243fa5adee8547f6d5dba6cddedc562751321
L-0-W <typeint@proton.me>
2026-02-11 10:29:59 -0300

    adicionando link para pagina que mostra informações que existem no Win32_Process
    Refactor open_process and close_process functions with improved comments and structure.

--- open-close.ps1
+++ open-close.ps1
@@ -8,8 +8,9 @@ function close_process {
     # Get-CimInstance - CIM e uma estrutura de classes para processos e serviocos do computador, tudo de informacoes de software e hardware
                       # CimInstance pega uma intancia da classe que deseja(abaixo e Win32_Process), entao estou pegando informacoes de todos os processos
                       # para depois usar um filtro, esse filtro e fala para apenas volta o process que tem o campo "ParentProcessId"(Id do Processo Pai) igual
-                      # ao id do processo que peguei em "open_process"
-     
+                      # ao id do processo que peguei em "open_process"                  
+                      # https://learn.microsoft.com/en-us/windows/win32/cimwin32prov/win32-process
+    
     # Motivo - Pelo que entendi, quando executei Start-Process "notepad", ele nao esta abrindo o notepad em si, mas um processo que inicializa o notepad e depois fecha.
              # Entao quando eu tentava fechar o processo pelo id pego na funcao acima, ele dizia que o id nao foi encontrado.
              
@@ -27,4 +28,5 @@ $process = open_process;
 Start-Sleep -Seconds 1
 Write-Output "Jaja fecho seu programa rsrs"
 Start-Sleep -Seconds 1
+
 close_process $process
